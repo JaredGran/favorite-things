018438a0b81a0706ffd863ea25f3ba5c32d668ba
JaredGran <jared.carper@gmail.com>
2022-06-06 11:33:39 -0700

    finished page #11 arrays
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>
    Co-authored-by: Seung Lee <shl7@uw.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,21 @@ $(document).ready(function() {
     let color = $("#color").val();
 
     const combinedFav = [animal, music, movie, color];
-    console.log(combinedFav);
-    $('#output').text(combinedFav);
+    const favHero = ["Iron Man"];
+    favHero.push(combinedFav[0], combinedFav[1], combinedFav[2]);
+   
+    $('#l1').text(favHero[0]);
+    $('#l2').text(favHero[1]);
+    $('#l3').text(favHero[2]);
+    $('#l4').text(favHero[3]);
     $("#animal").val('');
     $("#music").val('');
     $("#movie").val('');
     $("#color").val('');
+    
+    $('#output').show();
   });
 });
 
+
+// Finally, display this new array in your page's HTML as list items in a <ul> tag. 
